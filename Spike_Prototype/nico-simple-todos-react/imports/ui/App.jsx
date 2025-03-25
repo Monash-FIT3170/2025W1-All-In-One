53651358ace728e40c473a5106866515b07e7c5f
nico261204 <nwin0008@student.monash.edu>
2025-03-26 10:41:27 +1100

    nico's prototype

---
+++ Spike_Prototype/nico-simple-todos-react/imports/ui/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Task } from './task.jsx';
+
+const tasks = [
+ {_id: 1, text: 'Task Uno'},
+ {_id: 2, text: 'Task Dos'},
+ {_id: 3, text: 'Task Tres'},
+];
+
+
+export const App = () => (
+ <div>
+   <h1>Welcome to your TODO List!</h1>
+   <ul>
+     { tasks.map(task => <Task key={ task._id } task={ task }/>) } # These are the new changes.
+   </ul>
+ </div>
+);
+
