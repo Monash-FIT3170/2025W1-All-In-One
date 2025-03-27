7512adca2ce4bd09dd06a9d5f651e90867858b2a
Brandon <brandonluu.28@gmail.com>
2025-03-27 23:00:27 +1100

    Completed Basic List Tutorial

--- Spike_Prototype/brandon-simple-todos/imports/ui/App.jsx
+++ Spike_Prototype/brandon-simple-todos/imports/ui/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Task } from './Task.jsx';
+
+const tasks = [
+  {_id: 1, text: 'Task One - Install Meteor'},
+  {_id: 2, text: 'Task Two - Setup Meteor "simple-todos"'},
+  {_id: 3, text: 'Task Three - Create Tasks with Meteor App'},
+  {_id: 4, text: 'Task Four - Setup Tailwind CSS'},
+];
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <h1>Welcome to Brandon's Todo List</h1>
+    <ul>
+      { tasks.map(task => <Task key={ tasks._id } task = { task }/>) }
+      </ul>
   </div>
 );
