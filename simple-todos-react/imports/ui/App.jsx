4b98d2a20c43bc5ab8caf834c4c6cf381ece9da5
Charlotte <ceva0014@student.monash.edu>
2025-04-01 23:41:27 +1100

    testreactapp

---
+++ simple-todos-react/imports/ui/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Task } from './Task';
+
+
+const tasks = [
+ {_id: 1, text: 'Task Uno'},
+ {_id: 2, text: 'Task Duos'},
+ {_id: 3, text: 'Task Tres'},
+];
+
+
+export const App = () => (
+  <div>
+    <h1>Welcome to your TODO List!</h1>
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) } # These are the new changes.
+    </ul>
+  </div>
+);
