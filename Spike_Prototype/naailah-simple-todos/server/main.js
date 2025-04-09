1c6760c8578b805c7e0ab6b4456b7e93910b0757
nhas0021 <166671577+nhas0021@users.noreply.github.com>
2025-04-09 13:58:39 +0300

    created the login form jsx

--- Spike_Prototype/naailah-simple-todos/server/main.js
+++ Spike_Prototype/naailah-simple-todos/server/main.js
@@ -3,11 +3,21 @@ import { TasksCollection } from "/imports/api/TasksCollection";
 import "../imports/api/Publications"; 
 import "../imports/api/tasksMethods"; 
 
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
 const insertTask = (taskText) =>
   TasksCollection.insertAsync({ text: taskText });
 
 Meteor.startup(async () => {
   if ((await TasksCollection.find().countAsync()) === 0) {
+    if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
+      await Accounts.createUser({
+        username: SEED_USERNAME,
+        password: SEED_PASSWORD,
+      });
+    }
+    
     [
       "Task Uno",
       "Task Dos",
