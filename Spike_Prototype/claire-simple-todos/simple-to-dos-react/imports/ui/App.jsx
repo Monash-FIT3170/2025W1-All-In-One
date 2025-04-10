f5a32289c4482d7f257e4eb97092a5c04ae6df2e
clairezhangXD <clairez2015@gmail.com>
2025-04-10 23:34:09 +1000

    add login feature

--- Spike_Prototype/claire-simple-todos/simple-to-dos-react/imports/ui/App.jsx
+++ Spike_Prototype/claire-simple-todos/simple-to-dos-react/imports/ui/App.jsx
@@ -1,28 +1,58 @@
-import React from "react";
-import { useTracker, useSubscribe } from "meteor/react-meteor-data";
-import { TasksCollection } from "/imports/api/TasksCollection";
-import { Task } from "./Task";
-import { TaskForm } from "./TaskForm";
+import { Meteor } from 'meteor/meteor';
+import React, { useState, Fragment } from 'react';
+import { useTracker, useSubscribe } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { Task } from './Task';
+import { TaskForm } from './TaskForm';
+import { LoginForm } from './LoginForm';
 
 export const App = () => {
+
   const isLoading = useSubscribe("tasks");
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+
+  const user = useTracker(() => Meteor.user());
   
+  const tasks = useTracker(() =>
+    TasksCollection.find(
+      {},
+      { sort: { createdAt: 1 } }
+    ).fetch()
+  );
+  
+  const handleLogout = () => {
+    Meteor.logout();
+  };
 
   if (isLoading()) {
     return <div>Loading...</div>;
   }
   return (
-    <div>
-      <h1>Welcome to your TODO list!</h1>
+    <div className="main">
+        {user ? (
+          <Fragment>
+            <div className="header">
+              <h1>Welcome, {user.username}!</h1>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
 
-      <TaskForm />
+            <ul className="tasks">
+              {tasks.map(task => (
+                <Task
+                  key={task._id}
+                  task={task}
+                />
+              ))}
+            </ul>
 
-      <ul>
-        {tasks.map((task) => (
-          <Task key={task._id} task={task} />
-        ))}
-      </ul>
+            <div>
+              <TaskForm />
+            </div>
+
+          </Fragment>
+        ) : (
+          <LoginForm />
+        )}
     </div>
+    
   );
 };
--- Spike_Prototype/claire-simple-todos/simple-to-dos-react/server/main.js
+++ Spike_Prototype/claire-simple-todos/simple-to-dos-react/server/main.js
@@ -1,4 +1,5 @@
-import { Meteor } from "meteor/meteor";
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { TasksCollection } from "../imports/api/TasksCollection";
 import "../imports/api/TasksPublication";
 import "../imports/api/TasksMethods";  
@@ -6,7 +7,17 @@ import "../imports/api/TasksMethods";
 const insertTask = (taskText) =>
   TasksCollection.insertAsync({ text: taskText });
 
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
 Meteor.startup(async () => {
+  if (!(await Accounts.findUserByUsername(SEED_USERNAME))) {
+    await Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
   if ((await TasksCollection.find().countAsync()) === 0) {
     [
       "Task Uno",
