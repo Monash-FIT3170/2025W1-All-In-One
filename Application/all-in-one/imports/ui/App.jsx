a756cbc7cae360c0242cfd0688676e7a321d8a32
Naailah Hasan <hasannaailah@gmail.com>
2025-04-27 11:34:21 +0300

    Re-Installed meteor due to erroes. Connection between Main page and login

--- Application/all-in-one/imports/ui/App.jsx
+++ Application/all-in-one/imports/ui/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { LoginPage } from './LoginPage.jsx'; // your login page
 
 export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
+  <Router>
+    <div>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={
+          <div>
+            <h1>Home Page</h1>
+            <p>Already have an account? 
+              <Link to="/login" style={{ color: "blue", marginLeft: "5px" }}>
+                Login here
+              </Link>
+            </p>
+          </div>
+        } />
+
+        {/* Login Page */}
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  </Router>
 );
