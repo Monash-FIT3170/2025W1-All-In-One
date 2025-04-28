aefb65817f63d3a563b7c04f6c1bfdbac4e43283
srij0001 <srij0001@student.monash.edu>
2025-04-28 16:40:43 +1000

    Added navbar to the BookInspection page and linked it to the homepage

--- Application/all-in-one/imports/ui/App.jsx
+++ Application/all-in-one/imports/ui/App.jsx
@@ -7,16 +7,21 @@ import {
 } from 'react-router-dom';
 
 import { About } from './About.jsx';
+import { BookInspection } from './BookInspection.jsx'; // import new page
 
 export const App = () => (
   <Router>
     <div>
       <h1>Home Page</h1>
+      <div className="flex gap-4">
       <Link to="/about">Go to About Page</Link>
+      <Link to="/book-inspection">Book Inspection Page</Link> {/* New Link */}
+      </div>
 
       <Routes>
         <Route path="/" element={<div>Welcome to the home page!</div>} />
         <Route path="/about" element={<About />} />
+        <Route path="/book-inspection" element={<BookInspection />} /> {/* New Route */}
       </Routes>
     </div>
   </Router>
