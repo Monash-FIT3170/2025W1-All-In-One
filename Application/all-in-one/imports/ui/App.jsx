21c4bb3b424bfc6de3bc9c13baf310ff40a87718
Claire Zhang <czha0152@student.monash.edu>
2025-04-30 01:57:08 +1000

    Configure gitignore and remove generated directories from tracking and add Sign up page

--- Application/all-in-one/imports/ui/App.jsx
+++ Application/all-in-one/imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import { LoginPage } from './LoginPage.jsx'; // your login page
+import { SignUpPage } from './SignUpPage.jsx'; // your signup page
 
 export const App = () => (
   <Router>
@@ -16,11 +17,20 @@ export const App = () => (
                 Login here
               </Link>
             </p>
+            <p>Don't have an account? 
+              <Link to="/signup" style={{ color: "blue", marginLeft: "5px" }}>
+                Sign up here
+              </Link>
+            </p>
           </div>
         } />
 
         {/* Login Page */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* Sign Up Page */}
+        <Route path="/signup" element={<SignUpPage />} />
+
       </Routes>
     </div>
   </Router>
