d128bcadebe781e8d4a7cc85c1c6b1f49efd6237
Kenuli-W <kwij0022@student.monash.edu>
2025-05-08 11:44:49 +1000

    Linked add property listings page and added it to App.

---
+++ Application/all-in-one/imports/ui/AgentPropListing.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import { FaBath,FaBed, FaCar, FaCouch} from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
+
+export default function DetailedPropListing() {
+  // mock data- should be connected to database once its set up
+
+  const properties = [
+    {
+      id: 1,
+      location: "Melton South, 3338",
+      price: "$800",
+      image: "/images/Melton/melton_property_livingroom.png",
+      beds: 3,
+      baths: 2,
+      cars: 1,
+      living: 1
+    },
+    {
+      id: 2,
+      location: "Sydney, 2000",
+      price: "$620",
+      image: "/images/Sydney/Sydney_front.jpeg",
+      beds: 3,
+      baths: 2,
+      cars: 1,
+      living: 1
+    }
+  ];
+
+  return (
+    <div className="min-h-screen bg-[#FFF8EB] flex flex-col">
+      {/*Header*/}
+      <Navbar/>
+
+      {/* Page Heading */}
+      <div className="max-w-7xl mx-auto w-full px-6 mt-6">
+        <div className="pl-6">
+          <h1 className="text-3xl font-medium text-gray-800">View Properties</h1>
+          <p className="text-gray-600 text-base mt-1">All applications in one place!</p>
+          <hr className="my-6 border-t-2 border-gray-300 w-full" 
+          style={{ borderTop: '1px solid #e0e0e3', width: '1185px' }}/>
+        </div>
+      </div>
+      
+      {/* Search + Filters */}
+      <div className="mt-4 flex justify-center">
+        <div className="bg-[#CEF4F1] p-4 rounded-lg flex gap-4 w-full" style={{ maxWidth: '1185px' }}>
+          <input
+            type="text"
+            placeholder="Search Property..."
+            className="flex-1 px-4 py-2 rounded-md"
+            style={{ backgroundColor: '#fffcf7' }}
+          />
+
+          <select className="dropdown-arrow ml-auto rounded-lg" style={{ width: '200px',backgroundColor: '#FFFCF7', color: '#9da3ae' }}>
+            <option>Filter</option>
+          </select>
+        </div>
+      </div>
+      
+      <div className="mt-4 w-full flex justify-center">
+        <div className="w-[1220px] px-4 py-3 flex justify-start rounded-lg">
+          <button
+            className="px-6 py-2 text-white rounded-lg focus:outline-none"
+            style={{
+              backgroundColor: '#fbe698',
+              color: '#1f2937',
+            }}
+            onMouseOver={(e) => (e.currentTarget.style.backgroundColor = '#e6c86a')}
+            onMouseOut={(e) => (e.currentTarget.style.backgroundColor = '#fbe698')}
+          >
+            Add Property Listing
+          </button>
+        </div>
+      </div>
+
+
+      {/* Property Grid */}
+      <div className="mt-8 w-full flex justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-20 w-full max-w-[1230px] px-6">
+          {properties.map((property) => (
+            <Link 
+                              key={property.id}
+                              to={`/DetailedPropListing/${property.id}`}
+                              className="bg-white rounded-lg shadow-md overflow-hidden"
+                              style={{ backgroundColor: "#CEF"}}
+                              >
+              <img src={property.image} alt={property.location} className="w-full h-[250px] object-cover" />
+              <div className="p-4">
+                <div className="mt-2 flex justify-between items-center">
+                  <h2 className="text-3xl font-medium text-gray-500">
+                    {property.price} <span className="text-3xl font-medium text-gray-500">per week</span>
+                  </h2>
+                  <p className="text-gray-600 text-right">{property.location}</p>
+                </div>
+                  <div className="px-4 pb-4 mt-4 flex justify-between text-gray-600 text-sm">
+                    <div className="flex items-center gap-2">
+                      <FaBath size={24} />
+                      <span className="text-xl">{property.beds}</span>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <FaBed size={24} />
+                      <span className="text-xl">{property.beds}</span>
+                    </div>
+                    
+                    <div className="flex items-center gap-2">
+                      <FaCar size={24} />
+                      <span className="text-xl">{property.beds}</span>
+                    </div>
+                  </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      {/* Blank space before footer */}
+      <div className="h-40" />
+
+
+      {/*Footer*/}
+      <Footer/>
+
+
+    </div>
+  );
+}
--- Application/all-in-one/imports/ui/App.jsx
+++ Application/all-in-one/imports/ui/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import BasicPropListing from './BasicPropListing.jsx';
 import DetailedPropListing from './DetailedPropListing.jsx';
+import AgentPorpListing from './AgentPropListing.jsx';
+import AddPropertyListing from './AddPropertyListing.jsx';
 
 export default function App ()  {
   return(
@@ -9,6 +11,8 @@ export default function App ()  {
     <Routes>
       <Route path="/BasicPropListing" element={<BasicPropListing />} />
       <Route path="/DetailedPropListing/:id" element={<DetailedPropListing/>}/>
+      <Route path="/AgentPropListing" element={<AgentPorpListing/>}/>
+      <Route path="/AddPropertyListing" element={<AddPropertyListing/>}/>
       <Route path="/" element={<div>Welcome Page</div>} />
     </Routes>
   </Router>
