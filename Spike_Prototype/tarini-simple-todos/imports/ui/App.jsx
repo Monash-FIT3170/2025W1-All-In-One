27fa45c92e35ff5a6ab22d7e7f61105163eaa792
Tarini-M <tmeh0003@student.monash.edu>
2025-05-13 16:23:53 +1000

    Rendered basic task list with reusable Task component

--- Spike_Prototype/tarini-simple-todos/imports/ui/App.jsx
+++ Spike_Prototype/tarini-simple-todos/imports/ui/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Task } from './task';
+
+const tasks = [
+  { _id: 1, text: 'Task One' },
+  { _id: 2, text: 'Task Two' },
+  { _id: 3, text: 'Task Three' },
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <ul>
+      {tasks.map(task => (
+        <Task key={task._id} task={task} />
+      ))}
+    </ul>
   </div>
 );
