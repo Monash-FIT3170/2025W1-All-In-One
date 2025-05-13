35861bfa8e2ae6418d376d9080904302233e8b4e
Kenuli-W <kwij0022@student.monash.edu>
2025-05-14 09:35:55 +1000

    Added Landlord Header and footer

---
+++ Application/all-in-one/imports/ui/LandLord/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+    return (
+        <footer className="bg-[#CEF4F1] text-white h-[50px] py-4 mt-auto">
+        </footer>
+    );
+}
+
+export default Footer;
