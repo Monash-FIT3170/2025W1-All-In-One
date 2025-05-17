9aef85a25557d0305506261ff84b2ea0d3181d77
Kenuli-W <kwij0022@student.monash.edu>
2025-05-17 19:42:18 +1000

    Added image viewing option in the detailed view for properties with multiple images. Will need to be formatted more.

--- Application/all-in-one/imports/ui/globalComponents/PropertyDetailsCard.jsx
+++ Application/all-in-one/imports/ui/globalComponents/PropertyDetailsCard.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import { FaBath,FaBed, FaCar, FaCouch} from "react-icons/fa";
+import {useState} from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 //////////////////////////////////////////////////////////////////////////////////
 // Component used to display the Property details in the detailed property view //
 //////////////////////////////////////////////////////////////////////////////////
+function SampleNextArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      onClick={onClick}
+      className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black bg-opacity-50 hover:bg-opacity-80 text-white rounded-full p-2 cursor-pointer z-10"
+    >
+      <FaChevronRight size={20} />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      onClick={onClick}
+      className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black bg-opacity-50 hover:bg-opacity-80 text-white rounded-full p-2 cursor-pointer z-10"
+    >
+      <FaChevronLeft size={20} />
+    </div>
+  );
+}
 
 export default function PropertyDetailsCard({property}){
+
+  const defaultImage= "/images/default.jpg"
+
+  const [isModalOpen, setIsModalOpen]= useState(false);
+
     if (!property){
         return (
             <div classname="text-xl text-red-600">
@@ -14,21 +47,66 @@ export default function PropertyDetailsCard({property}){
             </div>
         );
     }
+
+    const openModal=()=> setIsModalOpen(true);
+    const closeModal=()=> setIsModalOpen(false);
+
     return(
+      <>
+      {/*Image Carousel*/}
+      {isModalOpen && property.imageUrls?.length > 0 &&(
+        <div className="fixed inset-0 bg-black/80 z-50 flex justify-center items-center p-4">
+          <div className="bg-black p-6 rounded-lg max-w-4xl w-full max-h-[90vh] overflow-y-auto shadow-lg">
+            <div className="w-full text-right mb-2">
+            <button
+            onClick={closeModal}
+            className="absolute top-2 right-4 text-gray-500 hover:text-black text-xl">
+            x
+            </button>
+            </div>
+             {property.imageUrls.length === 1 ? (
+        // Show single image without Slider
+        <div className="flex justify-center items-center">
+          <img
+            src={property.imageUrls[0]}
+            alt="Property Default"
+            className="max-h-[60vh] max-w-full object-contain"
+          />
+        </div>
+      ) :(<Slider dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1} arrows={true} nextArrow={<SampleNextArrow/>} prevArrow={<SamplePrevArrow/>}>
+              {property.imageUrls.map((url, index)=>(
+                <div key={index} className="flex justify-center items-center">
+                  <img src={url} alt={`Property ${index}`} className="max-h-[60vh] max-w-full object-contain"/>
+                </div>
+              ))}
+            </Slider>
+      )}
+          </div>
+        </div>
+      )}
+    
         <div className="flex flex-col lg:flex-row p-6">
           {/*Images*/}
           <div className="w-full lg:w-2/3">
             <div className="flex space-x-2">
               <div className="w-[100%] h-[30rem] overflow-hidden rounded-lg">
                 <img
-                  src={property.imageUrls[0]}
+                  src={property.imageUrls[0] || defaultImage}
                   alt="Property Image"
                   className="w-full h-full object-cover"
                 />
+                {/*View all images button*/}
+                <button 
+                onClick={openModal}
+                className="absolute bottom-4 right-4 bg-white text-gray-700 px-4 py-2 rounded-lg shadow hover:bg-gray-100">
+                  View All Images
+                </button>
               </div>
               <div className="flex flex-col space-y-2 w-[50%]">
-                {property.imageUrls.slice(1, 4).map((imageUrl, index) => (
-                  <div
+                {[...Array(3)].map((_, index)=>{
+                  const imageUrl= property.imageUrls[index+1]|| defaultImage;
+                  return(
+                    <div
                     key={index}
                     className="w-full h-[9.6rem] overflow-hidden rounded-lg"
                   >
@@ -38,8 +116,10 @@ export default function PropertyDetailsCard({property}){
                       alt={"Property image"}
                       className="w-full h-full object-cover"
                     />
-                  </div>
-                ))}
+                  </div> 
+                  );
+                })}
+
               </div>
             </div>
           </div>
@@ -81,5 +161,6 @@ export default function PropertyDetailsCard({property}){
             </div>
           </div>
         </div>
+        </>
     );
     }
