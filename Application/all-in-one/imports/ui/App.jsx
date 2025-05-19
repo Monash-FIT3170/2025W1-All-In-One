518ee4c3f4108774cf92ed501757f785a8ab9378
Claire Zhang <czha0152@student.monash.edu>
2025-05-20 00:09:23 +1000

    create browser router (use if change to using context for checking authentication)

--- Application/all-in-one/imports/ui/App.jsx
+++ Application/all-in-one/imports/ui/App.jsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { createBrowserRouter, BrowserRouter as Router, Routes, Route, Link, RouterProvider } from 'react-router-dom';
 
 import { LoginPage } from './LoginPage.jsx';
 import { HomePage } from './HomePage.jsx'; // Add this import
 import { SignUpPage } from './SignUpPage.jsx'; // your signup page
+import { Dashboard } from './Dashboard.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <HomePage />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  },
+  // Add other routes here
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider isSignedIn=false>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    <Router router={router} />
+  </React.StrictMode>,
+);
 
 export const App = () => (
-  <Router>
-    <div>
-      <Routes>
-        {/* Home Page */}
-        <Route path="/" element={<HomePage />} />
+  // <Router>
+  //   <div>
+  //     <Routes>
+  //       {/* Home Page */}
+  //       <Route path="/" element={<HomePage />} />
 
-        {/* Login Page */}
-        <Route path="/login" element={<LoginPage />} />
+  //       {/* Login Page */}
+  //       <Route path="/login" element={<LoginPage />} />
 
-        {/* Sign Up Page */}
-        <Route path="/signup" element={<SignUpPage />} />
+  //       {/* Sign Up Page */}
+  //       <Route path="/signup" element={<SignUpPage />} />
 
-        {/* You can add signup page later */}
-      </Routes>
-    </div>
-  </Router>
+  //       {/* Dashboard Page - Agent*/}
+  //       <Route path="/dashboard" element={<Dashboard/>} />
+  //     </Routes>
+  //   </div>
+  // </Router>  
 );
