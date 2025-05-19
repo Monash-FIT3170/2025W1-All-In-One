821d1b179e0c3edc5a21886ea30698b54d7056ee
Claire Zhang <czha0152@student.monash.edu>
2025-05-20 01:47:51 +1000

    update routing to enable protected routes

--- Application/all-in-one/imports/ui/App.jsx
+++ Application/all-in-one/imports/ui/App.jsx
@@ -1,57 +1,65 @@
 import React from 'react';
-import { createBrowserRouter, BrowserRouter as Router, Routes, Route, Link, RouterProvider } from 'react-router-dom';
-
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { LoginPage } from './LoginPage.jsx';
-import { HomePage } from './HomePage.jsx'; // Add this import
-import { SignUpPage } from './SignUpPage.jsx'; // your signup page
+import { HomePage } from './HomePage.jsx'; 
+import { SignUpPage } from './SignUpPage.jsx';
 import { Dashboard } from './Dashboard.jsx';
+import { SignedInTenant } from './SignedInTenant.jsx';
+import { SignedInLandlord } from './SignedInLandlord.jsx'; 
+import ProtectedAgentRoutes from './utils/ProtectedAgentRoutes.jsx'; 
+import ProtectedLandlordRoutes from './utils/ProtectedLandlordRoutes.jsx';
+import ProtectedTenantRoutes from './utils/ProtectedTenantRoutes.jsx'; 
 
-const router = createBrowserRouter([
-  {
-    path: '/', 
-    element: <HomePage />
-  },
-  {
-    path: '/login',
-    element: <LoginPage />
-  },
-  {
-    path: '/signup',
-    element: <SignUpPage />
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />
-  },
-  // Add other routes here
-
-])
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider isSignedIn=false>
-      <RouterProvider router={router} />
-    </AuthProvider>
-    <Router router={router} />
-  </React.StrictMode>,
-);
 
 export const App = () => (
-  // <Router>
-  //   <div>
-  //     <Routes>
-  //       {/* Home Page */}
-  //       <Route path="/" element={<HomePage />} />
+  <BrowserRouter>
+    <div>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<HomePage />} />
 
-  //       {/* Login Page */}
-  //       <Route path="/login" element={<LoginPage />} />
+        {/* Login Page */}
+        <Route path="/login" element={<LoginPage />} />
 
-  //       {/* Sign Up Page */}
-  //       <Route path="/signup" element={<SignUpPage />} />
+        {/* Sign Up Page */}
+        <Route path="/signup" element={<SignUpPage />} />
 
-  //       {/* Dashboard Page - Agent*/}
-  //       <Route path="/dashboard" element={<Dashboard/>} />
-  //     </Routes>
-  //   </div>
-  // </Router>  
+        {/* Protected Tenant Routes */}
+        <Route element={<ProtectedTenantRoutes />}>
+
+          {/* Signed In Tenant Page */}
+          <Route path="/signedInTenant" element={<SignedInTenant />} />
+          
+          {/* Add any other protected tenant routes here */}
+
+
+
+        </Route>
+
+        {/* Protected Agent Routes */}
+        <Route element={<ProtectedAgentRoutes />}>
+          
+          {/* Dashboard Page*/}
+          <Route path="/dashboard" element={<Dashboard/>} />
+        
+          {/* Add any other protected agent routes here */}
+        
+
+
+        </Route>
+        
+        {/* Protected Landlord Routes */}
+        <Route element={<ProtectedLandlordRoutes />}>
+          {/* Signed In Landlord Page*/}
+          <Route path="/signedInLandlord" element={<SignedInLandlord/>} />
+
+          {/* Add any other protected routes here */}
+        
+
+
+        </Route> 
+
+      </Routes>
+    </div>
+  </BrowserRouter>  
 );
