75714cb48523cb59c4862e4e4f3c7e1196e25aad
breadluu <brandonluu.28@gmail.com>
2025-05-21 10:10:26 +1000

    Fixed method to add to properties database

--- Application/all-in-one/imports/api/methods/account.js
+++ Application/all-in-one/imports/api/methods/account.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import { Tenants } from '../database/collections.js'; // adjust path if needed
+import { Tenants, Properties } from '../database/collections.js'; // adjust path if needed
 
 Meteor.methods({
   async registerUser({ email, password, firstName, lastName, role }) {
@@ -33,5 +33,40 @@ Meteor.methods({
       console.error("registerUser error:", error);
       throw new Meteor.Error("register-failed", error.message || "Something went wrong.");
     }
-  }
+  },
+
+  async "addProperty" ({propAddress, pricePerWeek, numBeds, numBaths, numParkSpots, propType, description, dateAvailable, isFurnished, petsAllowed, bond, status}) {
+    try {
+      {/* Creating Property Id */}
+      const collectionSize = await Properties.find({}, {fields: {prop_id: 1}}).countAsync() + 1;
+      const idNum = String(collectionSize).padStart(3, '0');
+      const propID = "P" + idNum;
+      const date = new Date(dateAvailable);
+   
+      {/* Adding Property to 'Properties' Database */}
+      await Properties.insertAsync({
+        prop_id: propID,
+        prop_address: propAddress,
+        prop_pricepweek: pricePerWeek,
+        prop_numbeds: numBeds,
+        prop_numbaths: numBaths,
+        prop_numcarspots: numParkSpots,
+        prop_type: propType,
+        prop_desc: description,
+        prop_available_date: date,
+        prop_furnish: isFurnished,
+        prop_pets: petsAllowed,
+        prop_bond: bond,
+        prop_status: status,
+        agent_id: 'A001',   // to be changed
+        landlord_id: 'L001' // to be changed
+      });
+  
+      return propID;
+    } catch (error) {
+      console.error("addProperty error:", error);
+      throw new Meteor.Error("add-property-failed", error.message || "Something went wrong.");
+    }
+  },
+  
 });
