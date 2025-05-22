09dfc1595f98d4687ed0eddf11ef36a0cf71b42a
Kenuli-W <kwij0022@student.monash.edu>
2025-05-22 18:19:18 +1000

    Added nav bar button component for ease of extension.
    Edited agent nav bar with new component and comments

--- Application/all-in-one/imports/ui/agent/components/AgentNavbar.jsx
+++ Application/all-in-one/imports/ui/agent/components/AgentNavbar.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React from "react";
+import { Link } from "react-router-dom";
+import NavButton from "../../globalComponents/NavButton";
 
 function Navbar() {
   return (
     <div className="flex items-center justify-between px-6 h-[63px] bg-[#CBADD8]">
-      <Link to="/" className="flex items-center gap-2 h-full">
+      {/*Logo linked back to search bar*/}
+      <Link
+        to="/AgentBasicPropListing"
+        className="flex items-center gap-2 h-full"
+      >
         <img src="/images/logo.png" alt="Logo" className="h-12" />
-      </Link> 
+      </Link>
+
+      {/*Nav bar options*/}
       <div className="flex items-center gap-4">
-        {['Dashboard', 'Applications', 'Properties'].map((label) => (
-          label === 'Properties' ? (
-            <Link
-              key={label}
-              to="/AgentListings"
-              className="bg-[#9747FF] text-white px-4 py-1.5 rounded-full font-base hover:bg-violet-900 transition"
-            >
-              {label}
-            </Link>
-          ) : (
-            <button
-              key={label}
-              className="bg-[#9747FF] text-white px-4 py-1.5 rounded-full font-base hover:bg-violet-900 transition"
-            >
-              {label}
-            </button>
-          )
-        ))}
-        <button className="bg-[#9747FF] text-white px-4 py-1.5 rounded-full font-base hover:bg-violet-900 transition">
-          Log out
-        </button>
-        <img src="/images/user-avatar.png" alt="User Avatar" className="w-10 h-10 rounded-full" />
+        <NavButton to="/PLACEHOLDER">Dashboard</NavButton> {/*Add route to Agent dashboard*/}
+        <NavButton to="/PLACEHOLDER">Applications</NavButton> {/*Add route to Agent applications*/}
+        <NavButton to="/AgentListings">Properties</NavButton> 
+        <NavButton onClick={() => console.log("Logout clicked")}> {/*Add logout function*/}
+          Log Out
+        </NavButton>
+
+        <Link to="/PLACEHOLDER"> {/*Add route to the Agent's profile settings*/}
+          <img
+            src="/images/user-avatar.png"
+            alt="User Avatar"
+            className="w-10 h-10 rounded-full"
+          />
+        </Link>
       </div>
     </div>
   );
