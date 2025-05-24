b0909a179f6fd0598283b8e2f39ded8987a93e78
Kenuli-W <kwij0022@student.monash.edu>
2025-05-24 14:38:22 +1000

    Added link to login in guest book inspections and application

--- Application/all-in-one/imports/ui/globalComponents/DetailedPropListing.jsx
+++ Application/all-in-one/imports/ui/globalComponents/DetailedPropListing.jsx
@@ -76,12 +76,12 @@ export default function DetailedPropListing() {
         <PropertyDetailsCard property={propertyData} />
         <div className="w-full flex flex-row gap-4 mb-8 pt-10">
           <Link
-          to={`/PLACEHOLDER`} // TBD: replace with link to login page
+          to={`/login`}
           className="w-1/2 bg-[#9747FF] hover:bg-violet-900 text-white font-base text-center py-2 rounded-md shadow-md transition duration-200"
           >Book Inspection 
           </Link>
           <Link
-          to={`/PLACEHOLDER`} // TBD: replace with link to login page
+          to={`/login`} 
           className="w-1/2 bg-[#9747FF] hover:bg-violet-900 text-white font-base text-center py-2 rounded-md shadow-md transition duration-200"
           >Apply 
           </Link>
