04370074596a52a433480c674d2646553b23fcee
Brandon Luu <brandonluu.28@gmail.com>
2025-08-24 23:38:37 +1000

    Redirect guests to log in page when choosing an open house timeslot, or sending EOI

--- Application/all-in-one/imports/ui/tenant/TenDetailedPropListing.jsx
+++ Application/all-in-one/imports/ui/tenant/TenDetailedPropListing.jsx
@@ -15,72 +15,72 @@ import UpcomingOpenHouseModal from "./components/UpcomingOpenHouseModal";
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export default function TenDetailedPropListing() {
+
+  const[openModal, setOpenModal] = useState(false);
+  const { id } = useParams();
+  console.log("propId received:", id);
+  
+  const { isReady, property, photos, videos, openHouses, starredProperties}=  useTracker(()=>{
+    const tenantID = Meteor.userId();
+    const subProps = Meteor.subscribe("properties");
+    const subPhotos = Meteor.subscribe("photos");
+    const subVideos = Meteor.subscribe("videos");
+    const subAvailabilities = Meteor.subscribe("agentAvailabilities")
+    const subStarred = Meteor.subscribe("starredProperties"); 
+
+    const isReady= subProps.ready() && subPhotos.ready() && subVideos.ready() && subAvailabilities.ready() && subStarred.ready();
+
+    let property= null;
+    let photos= [];
+    let videos=[];
+    let openHouses = [];
+    let starredProperties = [];
+
+    // find property, photos and videos corresponding to the property ID passed.
+    if (isReady){
+      property= Properties.findOne({prop_id: id});
+      photos= Photos.find({prop_id: id}, {sort:{photo_order:1}}).fetch();
+      videos= Videos.find({prop_id: id}).fetch();
+      starredProperties = StarredProperties.find({tent_id: tenantID, prop_id: id}).fetch();
+      openHouses = AgentAvailabilities.find({type: "Open House"}).fetch()
+      openHouses = openHouses.filter((p) => p.property.address === property.prop_address)
+    }
+
+    return {isReady, property, photos, videos, openHouses, starredProperties};
+  }, [id]);
+
+  if (!isReady){
+    return (<div className="min-h-screen flex items-center justify-center text-xl text-gray-600">Loading Properties...</div>);
+  }
+
+  if (!property){
+    return (<div className="min-h-screen flex items-center justify-center text-xl text-red-600">Property Not Found!</div>);
+  }
+
+  const isStarred = starredProperties.length>0;
     
-    const[openModal, setOpenModal] = useState(false);
+  // data passed on to propertyDetailsCard
+  const propertyData= {
+    id: property.prop_id,
+    address: property.prop_address,
+    price:property.prop_pricepweek,
+    type:property.prop_type,
+    AvailableDate: property.prop_available_date,
+    Pets: property.prop_pets ? "True":"False",
+    imageUrls: photos.length? photos.map((photo)=>photo.photo_url):["/images/default.jpg"],
+    videoUrls: videos.length ? videos.map((video) => video.video_url) : [],
+    details:{
+      beds: property.prop_numbeds ?? "N/A",
+      baths: property.prop_numbaths ?? "N/A",
+      carSpots: property.prop_numcarspots ?? "N/A",
+      furnished: property.prop_furnish? "Yes":"No",
+    },
+    description: property.prop_desc,
+    starred: isStarred,
+  };
 
-    const { id } = useParams();
-    console.log("propId received:", id);
-    const { isReady, property, photos, videos, openHouses, starredProperties}=  useTracker(()=>{
-      const tenantID = Meteor.userId();
-      const subProps = Meteor.subscribe("properties");
-      const subPhotos = Meteor.subscribe("photos");
-      const subVideos = Meteor.subscribe("videos");
-      const subAvailabilities = Meteor.subscribe("agentAvailabilities")
-      const subStarred = Meteor.subscribe("starredProperties"); 
-
-      const isReady= subProps.ready() && subPhotos.ready() && subVideos.ready() && subAvailabilities.ready() && subStarred.ready();
-
-      let property= null;
-      let photos= [];
-      let videos=[];
-      let openHouses = [];
-      let starredProperties = [];
-
-      // find property, photos and videos corresponding to the property ID passed.
-      if (isReady){
-        property= Properties.findOne({prop_id: id});
-        photos= Photos.find({prop_id: id}, {sort:{photo_order:1}}).fetch();
-        videos= Videos.find({prop_id: id}).fetch();
-        starredProperties = StarredProperties.find({tent_id: tenantID, prop_id: id}).fetch();
-        openHouses = AgentAvailabilities.find({type: "Open House"}).fetch()
-        openHouses = openHouses.filter((p) => p.property.address === property.prop_address)
-      }
-        
-      return {isReady, property, photos, videos, openHouses, starredProperties};
-    }, [id]);
-
-      if (!isReady){
-        return (<div className="min-h-screen flex items-center justify-center text-xl text-gray-600">Loading Properties...</div>);
-      }
-
-      if (!property){
-        return (<div className="min-h-screen flex items-center justify-center text-xl text-red-600">Property Not Found!</div>);
-      }
-
-      const isStarred = starredProperties.length>0;
-    
-      // data passed on to propertyDetailsCard
-      const propertyData= {
-          id: property.prop_id,
-          address: property.prop_address,
-          price:property.prop_pricepweek,
-          type:property.prop_type,
-          AvailableDate: property.prop_available_date,
-          Pets: property.prop_pets ? "True":"False",
-          imageUrls: photos.length? photos.map((photo)=>photo.photo_url):["/images/default.jpg"],
-          videoUrls: videos.length ? videos.map((video) => video.video_url) : [],
-          details:{
-          beds: property.prop_numbeds ?? "N/A",
-          baths: property.prop_numbaths ?? "N/A",
-          carSpots: property.prop_numcarspots ?? "N/A",
-          furnished: property.prop_furnish? "Yes":"No",
-          },
-          description: property.prop_desc,
-          starred: isStarred,
-        };
-        
-      const tenantID = Meteor.userId()
-      console.log(tenantID)
+  const tenantID = Meteor.userId()
+  console.log(tenantID)
 
   return (
     <div className="min-h-screen bg-[#FFF8E9] flex flex-col">
