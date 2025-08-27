3fed97b46b0953bc7cbf6b2b7502aabe2c9795a4
Brandon Luu <brandonluu.28@gmail.com>
2025-08-27 21:57:43 +1000

    Filtered out the private open house

--- Application/all-in-one/imports/ui/tenant/TenDetailedPropListing.jsx
+++ Application/all-in-one/imports/ui/tenant/TenDetailedPropListing.jsx
@@ -42,8 +42,8 @@ export default function TenDetailedPropListing() {
       photos= Photos.find({prop_id: id}, {sort:{photo_order:1}}).fetch();
       videos= Videos.find({prop_id: id}).fetch();
       starredProperties = StarredProperties.find({tent_id: tenantID, prop_id: id}).fetch();
-      openHouses = AgentAvailabilities.find({type: "Open House"}).fetch()
-      openHouses = openHouses.filter((p) => p.property.address === property.prop_address)
+      openHouses = AgentAvailabilities.find({type: "Open House", is_private: false}).fetch();
+      openHouses = openHouses.filter((p) => p.property.address === property.prop_address);
     }
 
     return {isReady, property, photos, videos, openHouses, starredProperties};
