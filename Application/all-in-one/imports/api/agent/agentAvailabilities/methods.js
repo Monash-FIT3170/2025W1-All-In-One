2ed21b846eda34ea711242309f25b545c57d88d5
Vanshika Gupta <vgup0011@student.monash.edu>
2025-08-30 00:34:37 +1000

    addded errors for the delete and update pop up

--- Application/all-in-one/imports/api/agent/agentAvailabilities/methods.js
+++ Application/all-in-one/imports/api/agent/agentAvailabilities/methods.js
@@ -96,7 +96,36 @@ Meteor.methods({
   async 'agentAvailabilities.clear'() {
     console.log('Clearing all availabilities...');
     return await AgentAvailabilities.removeAsync({});
+  },
+  async 'agentAvailabilities.update'(availabilityId, updateData) {
+    check(availabilityId, String);
+    check(updateData, Object);
+    const result = await AgentAvailabilities.updateAsync(
+      { _id: availabilityId },
+      { $set: updateData }
+    );
+
+    if (result === 0) {
+      throw new Meteor.Error('not-found', 'No matching availability found.');
+    }
+
+    return result;
+  },
+  async 'agentAvailabilities.remove'(availabilityId) {
+    check(availabilityId, String);
+
+    const result = await AgentAvailabilities.removeAsync({ _id: availabilityId });
+
+    if (result === 0) {
+      throw new Meteor.Error('not-found', 'No matching availability found.');
+    }
+
+    return result;
   }
+
+
+
+  
 
 });
 
