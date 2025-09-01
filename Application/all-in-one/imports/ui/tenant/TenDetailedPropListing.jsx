8a189dadad65597a8b2ba211fcf9313ee739937a
Kenuli-W <kwij0022@student.monash.edu>
2025-09-01 21:54:20 +1000

    Typo in filtering saved listings. Was using tent_id instead of ten_id

--- Application/all-in-one/imports/ui/tenant/TenDetailedPropListing.jsx
+++ Application/all-in-one/imports/ui/tenant/TenDetailedPropListing.jsx
@@ -40,7 +40,7 @@ export default function TenDetailedPropListing() {
       property= Properties.findOne({prop_id: id});
       photos= Photos.find({prop_id: id}, {sort:{photo_order:1}}).fetch();
       videos= Videos.find({prop_id: id}).fetch();
-      starredProperties = StarredProperties.find({tent_id: tenantID, prop_id: id}).fetch();
+      starredProperties = StarredProperties.find({ten_id: tenantID, prop_id: id}).fetch();
       openHouses = AgentAvailabilities.find({type: "Open House", is_private: false}).fetch();
       openHouses = openHouses.filter((p) => p.property.address === property.prop_address);
     }
