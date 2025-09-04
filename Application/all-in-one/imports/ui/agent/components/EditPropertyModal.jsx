5d29eb40e75ce85ab20b68c1a96afbea47428cba
Brandon Luu <brandonluu.28@gmail.com>
2025-09-04 22:11:30 +1000

    Switched landlord search to non-async, and will search for _id

--- Application/all-in-one/imports/ui/agent/components/EditPropertyModal.jsx
+++ Application/all-in-one/imports/ui/agent/components/EditPropertyModal.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 
 function EditPropertyModal({isOpen, onClose, propertyData}) {
 
-  const currLandlordEmail = (Landlord.findOneAsync({ll_id: propertyData.landlord})).ll_email;
+  const currLandlordEmail = (Landlord.findOne({_id: propertyData.landlord})).ll_email;
 
   const propId = propertyData.id;
   const status = propertyData.status;
