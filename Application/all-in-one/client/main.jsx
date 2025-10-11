ed8979e1d7dda843bc27d1a3b398d505ba421005
Kenuli-W <kwij0022@student.monash.edu>
2025-10-12 01:57:12 +1100

    Entered onResetPasswordLink method to clients main,jxs.
    modern react setups dont need it, just legacy meteor setups do. But threw errors without it so it was ultimately included.

--- Application/all-in-one/client/main.jsx
+++ Application/all-in-one/client/main.jsx
@@ -4,6 +4,12 @@ import { createRoot } from 'react-dom/client';
 import App from '../imports/ui/App';
 import './main.css';
 
+// clicking the email link redirects to  ResetPassword page
+Accounts.onResetPasswordLink((token, done) => {
+  window.location.href = `/reset-password/${token}`;
+});
+
+
 Meteor.startup(() => {
   const root = createRoot(document.getElementById('react-target'));
   root.render(<App />);
