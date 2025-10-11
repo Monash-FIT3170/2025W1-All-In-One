b4dd1f74191d5d07b916582c5c916e3903e89700
Kenuli-W <kwij0022@student.monash.edu>
2025-10-12 01:57:36 +1100

    Import email template

--- Application/all-in-one/server/main.js
+++ Application/all-in-one/server/main.js
@@ -87,6 +87,7 @@ import '/imports/api/tenant/tickets/ticketsPublications'
 import '/imports/api/methods/eoi.js';
 import '/imports/api/agent/ticketActivities/methods.js';
 import '/imports/api/agent/ticketActivities/publications.js';
+import '/imports/api/emailTemplates.js';
 
 
 import '/imports/api/methods/profileSettings.js';
